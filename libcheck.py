843cd421a4f051f09197a7c3b2523e3585366c1e
Kevin <ktyers@gmail.com>
2018-10-17 10:43:05 -0700

    Update libcheck.py

--- libcheck.py
+++ libcheck.py
@@ -6,9 +6,12 @@ parser.add_argument('--port', '-p', default = 22, help="The port to check, leave
 parser.add_argument('ips', nargs='+', help="The IPs you want to check to see if they are vulnerable.")
 args = parser.parse_args()
 for ip in args.ips:
-    output = subprocess.check_output('nc -nw 3 {} {}'.format(ip, args.port), shell=True).decode()
-    if 'libssh' in output:
-        if not '8.4' in output and not '7.6' in output:
-            print('{} looks vulnerable'.format(ip))
-    else:
-        print('{} may not be vulnerable'.format(ip))
+    try:
+        output = subprocess.check_output('nc -nw 3 {} {}'.format(ip, args.port), shell=True).decode()
+        if 'libssh' in output:
+            if not '8.4' in output and not '7.6' in output:
+                print('{} looks vulnerable'.format(ip))
+        else:
+            print('{} may not be vulnerable'.format(ip))
+    except:
+        print("{} unable to connect".format(ip))
